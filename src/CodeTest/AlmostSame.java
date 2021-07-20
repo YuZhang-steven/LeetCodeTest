7f4374f1fe6f541ccb93a43ee15632d00e2f390e
Yu Zhang <zengeryounke@hotmail.com>
2021-07-19 17:42:54 -0700

    initial

---
+++ src/CodeTest/AlmostSame.java
@@ -0,0 +1,16 @@
+package CodeTest;
+
+public class AlmostSame {
+    public static int countEqual(int[] numbers) {
+        return -1;
+    }
+
+
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] test1 = new int[]{122,121,112,221};
+        int result = countEqual(test1);
+        System.out.println(result);
+    }
+}
